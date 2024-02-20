655c2e64d693ff5fc8325913711a8456367e1ca0
Tom Grossi <TheAutomaTom@gmail.com>
2024-02-19 19:38:16 -0600

    Add mvp Elasticsearch

--- Elk8.Lab.Api/Controllers/WeatherForecastController.cs
+++ Elk8.Lab.Api/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using Elastic.Clients.Elasticsearch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Elk8.Lab.Api.Controllers
@@ -6,32 +7,55 @@ namespace Elk8.Lab.Api.Controllers
   [Route("[controller]/[action]")]
   public class WeatherForecastController : ControllerBase
   {
-    private static readonly string[] Summaries = new[]
+    readonly IConfiguration _config;
+    readonly ElasticsearchClient _client;
+    readonly string _defaultIndex = "my_index";
+    readonly ILogger<WeatherForecastController> _logger;
+    readonly Random _rando = new Random();
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration config)
     {
-          "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+      _config = config;
+      _logger = logger;
+
+      var url = config.GetValue<string>("Elastic:Url");
+      var settings = new ElasticsearchClientSettings(new Uri(url));
+
+      _client = new ElasticsearchClient(settings);
+
+
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateRando()
+    {
+      var wf = new WeatherForecast()
+      {
+        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(_rando.Next(365))),
+        TemperatureC = Random.Shared.Next(-20, 55),
+        Summary = _summaries[Random.Shared.Next(_summaries.Length)],
       };
 
-    private readonly ILogger<WeatherForecastController> _logger;
+      var response = await _client.IndexAsync(wf, _defaultIndex);
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
-      _logger = logger;
-    }
-
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
-    {
-      _logger.LogInformation( "Information!");
-      _logger.LogWarning(     "LogWarning!");
-      _logger.LogCritical(    "LogCritical!");
-
-      return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+      if (response.IsValidResponse)
       {
-        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-        TemperatureC = Random.Shared.Next(-20, 55),
-        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-      })
-      .ToArray();
+        return Ok(response.Result);
+      }
+      return BadRequest(response.Result);
+
     }
+
+
+
+
+
+
+
+
+    static readonly string[] _summaries = new[]
+    {
+          "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
   }
 }
